339910496aa97ee53416900ecdcfbde1a0e4d118
srujan <srujanrimmalapudi2198@gmail.com>
2021-01-21 21:04:10 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Greet from './components/Greet'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Welcome to reactjs</h1>
+    <Greet />
+    </div>
+  );
+}
+
+export default App;
